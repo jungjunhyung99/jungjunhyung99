cac988fdb112d232869518ad03fae437d9009042
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2025-02-21 19:28:57 +0900

    [bugfix] 글이 중복되는 현상 수정정

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -20,7 +20,8 @@ const parser = new Parser({
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
   let latestPosts = "";
   for (let i = 0; i < 5 && i < feed.items.length; i++) {
-    const { title, link } = feed.items[i];
+    let { title, link } = feed.items[i];
+    link = link.startsWith("http://") ? "https://" + link.slice(7) : link;
     latestPosts += `- [${title}](${link})\n`;
   }
 
